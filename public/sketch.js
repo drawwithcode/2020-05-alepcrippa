f7da7e4287ed43c5d10c7c549a5c88c16833c268
alepcrippa <ale.crippa96@gmail.com>
2020-12-02 00:46:07 +0100

    save button and other adjustments

--- public/sketch.js
+++ public/sketch.js
@@ -6,18 +6,17 @@ let strokeColor = "black";
 let strokeHue = 0;
 let strokeBright = 0;
 let rubClicked=false;
-// let backHue = 180;
-// let backBright = 100;
+let cnv;
 let backColor = "white";
 let strokeW = 0.5;
 let backColorPalette = [ "WhiteSmoke", "Seashell","Honeydew", "LavenderBlush","AntiqueWhite", "Azure", "MistyRose" ]
+let edge = 50; //size of the circle area each particle is allowed to move
+let drawing = false; // I want to raw only when I click
 
 socket.on("connect", newConnection);
 socket.on("mouseBroad", drawOtherMouses);
 
-let edge = 50; //size of the circle area each particle is allowed to move
-let drawing = false; // I want to raw only when I click
-
+//drawing what clients draw
 function drawOtherMouses(mouseData){
   if(mouseData.rub==false){
     push();
@@ -35,74 +34,72 @@ function drawOtherMouses(mouseData){
   }
 }
 
+//not necessary
 function newConnection(){
   console.log("my connection: " + socket.id);
 }
 
+//set up the board
 function setup() {
   createCanvas(windowWidth,windowHeight);
   colorMode(HSB);
-  // colorBack = color(backHue, 50, backBright);
   backColor=random(backColorPalette);
-  background(backColor);//(80,100,240);
-
+  background(backColor);
+  //main color
   myColor=color(myHue,100,myBright)
-
   // checkboxes
   checkbox1 = createCheckbox('main color', true);
   checkbox1.position(windowWidth - 125, 5);
-
   checkbox1.style('font-family:Ubuntu');
   checkbox2 = createCheckbox('stroke color', false);
   checkbox2.position(windowWidth - 125, 25);
   checkbox2.style('font-family:Ubuntu');
-  // checkbox3 = createCheckbox('background & refresh', false);
-  // checkbox3.position(windowWidth - 95, 80);
-  // checkbox3.style('font-family:Ubuntu');
-  //slider stroke
+  //stroke slider
   sliderStroke = createSlider(0,20, 10);
   sliderStroke.position(windowWidth - 120, 420);
   sliderStroke.size(90);
   textSize(17);
   textFont("Ubuntu")
-  // text("stroke", windowWidth - 95, 390+75);
-  //slider edge
+  //dimension slider
   sliderEdge = createSlider (10, 120, 50);
   sliderEdge.position(windowWidth - 120, 460);
   sliderEdge.size(90);
-  //text("branches \n dimension", windowWidth - 95, 435+75);
-
+  //rubber
   sliderRub = createSlider(10,100,40);
   sliderRub.position(windowWidth - 90, 370);
   sliderRub.size(60);
-
-rub=createImg("rubber.png")
-rub.style("width:20px; position:absolute;  filter: grayscale(80%)")
-rub.position(windowWidth-115,370)
-rub.mouseClicked(myFunction);
-
+  rub=createImg("rubber.png")
+  rub.style("width:20px; position:absolute;  filter: grayscale(80%); opacity:0.6")
+  rub.position(windowWidth-115,370)
+  rub.mouseClicked(myFunction);
+  //downloading image
+  save = createButton("save");
+  save.position(windowWidth-100, 500);
+  save.mousePressed(download);
+  //building the toolbar
   setUp();
 }
 
+function download(){
+  let to_save = get( 0, 0, windowWidth-131, windowHeight );
+  to_save.save("saved_name", "png");
+}
+
+//rubber click
 function myFunction(){
-  // console.log("rubber clicked")
   if (rubClicked == false){
     rubClicked = true;
-    rub.style("filter: grayscale(0%)")
-    console.log("rub clicked");
+    rub.style("filter: grayscale(0%); opacity: 1")
   } else {
     rubClicked = false;
-    rub.style("filter: grayscale(80%)")
-    console.log("rub unclicked")
+    rub.style("filter: grayscale(80%);opacity:0.6")
   }
-
 }
 
 function draw() {
-
   strokeW = sliderStroke.value()/20;
   edge = sliderEdge.value();
-
+  //overriding text at every frame in order to have the right font
   push();
     fill('FloralWhite');
     noStroke();
@@ -116,89 +113,69 @@ function draw() {
     fill(0);
     text("dimension", windowWidth - 75, 460);
   pop();
-
-//setUp()
  }
 
+//selecting the color
 function mousePressed(){
   if((mouseX > windowWidth-115) && (mouseX < windowWidth-30) && (mouseY > 60) && (mouseY < 360)){
     if (checkbox1.checked()){
       myHue = map(mouseY, 60, 360, 0, 360);
       myBright = map(mouseX,windowWidth-115,windowWidth-30,100,0);
       myColor = color(myHue, 100, myBright);
-      //checkbox1.checked(t)
-      //checkbox2.checked(false)
-      // checkbox3.checked(false)
     }
     if (checkbox2.checked()){
       strokeHue = map(mouseY, 60, 360, 0, 360);
       strokeBright = map(mouseX,windowWidth-115,windowWidth-30,100,0);
       strokeColor = color(strokeHue, 100, strokeBright);
-      //checkbox1.checked(false)
-    //  checkbox2.checked(false)
-    //  checkbox3.checked(false)
     }
-    // if (checkbox3.checked()){
-    //   backHue = map(mouseY, 55+75, 355+75, 0, 360);
-    //   backBright = map(mouseX,windowWidth-90,windowWidth-25,100,0);
-    //   backColor = color(backHue, 50, backBright);
-    //   background(backColor);
-    //   checkbox1.checked(false)
-    //   checkbox2.checked(false)
-    //   checkbox3.checked(false)
-
-    // setUp();
-    // }
   }
+}
 
-}//end mousePressed
-
+//drawing
 function mouseDragged(){
-  if (mouseX<windowWidth - 100 - edge){
-    if (rubClicked==false){
-    // let message = {
-    //   x: mouseX,
-    //   y: mouseY,
-    //   h: myHue,
-    //   b: myBright,
-    //   sh: strokeHue,
-    //   sb: strokeBright,
-    //   sw: strokeW,
-    //   e: edge,
-    // };
+  if (rubClicked==false){
+    if (mouseX<windowWidth - 130 - edge){
     branchOut(mouseX,mouseY,myColor,strokeColor,strokeW,edge);
-
-  } else{
-    let margin = 100+sliderRub.value();
-    let boh=windowWidth-margin;
-    if(mouseX<boh ){
+    let message = {
+      x: mouseX,
+      y: mouseY,
+      px: pmouseX,
+      py: pmouseY,
+      r: sliderRub.value(),
+      rub: rubClicked,
+      h: myHue,
+      b: myBright,
+      sh: strokeHue,
+      sb: strokeBright,
+      sw: strokeW,
+      e: edge,
+    };
+    socket.emit("mousee", message)
+  }
+  } else
+    if(mouseX<windowWidth-130-sliderRub.value() ){
     push();
         stroke(backColor);
         strokeWeight(sliderRub.value());
           line(mouseX, mouseY, pmouseX, pmouseY);
     pop();
-        console.log(boh)
-      }
-
-      }
-      let message = {
-        x: mouseX,
-        y: mouseY,
-        px: pmouseX,
-        py: pmouseY,
-        r: sliderRub.value(),
-        rub: rubClicked,
-        h: myHue,
-        b: myBright,
-        sh: strokeHue,
-        sb: strokeBright,
-        sw: strokeW,
-        e: edge,
-      };
-      socket.emit("mousee", message)
-  }//if inside canvas
-}//mouseDragged
-
+    let message = {
+      x: mouseX,
+      y: mouseY,
+      px: pmouseX,
+      py: pmouseY,
+      r: sliderRub.value(),
+      rub: rubClicked,
+      h: myHue,
+      b: myBright,
+      sh: strokeHue,
+      sb: strokeBright,
+      sw: strokeW,
+      e: edge,
+    };
+    socket.emit("mousee", message)
+  }
+}//mouseDragged end
 
 class Root {
   constructor(x, y, color, strokeCol, strokeWe, centerX, centerY, edg){
@@ -236,6 +213,7 @@ class Root {
   }
 }
 
+//show the branches from every point
 function branchOut(_x, _y, _col, _strokeCol, _strokeW, _edge){
   push();
     for (let i = 0; i < 3; i++){
@@ -245,13 +223,12 @@ function branchOut(_x, _y, _col, _strokeCol, _strokeW, _edge){
   pop();
 }
 
-
+//setting up toolbar and color palette
 function setUp(){
   push();
     //toolbar
     fill('FloralWhite');
     rect(windowWidth-130,0,115,windowHeight);
-
     //color palette
     for(let i = 0; i < 300; i++){
       let hue = map(i,0,300,0,360);
@@ -263,13 +240,4 @@ function setUp(){
       }
     }
   pop();
-
-
-  // push();
-  //   fill(0)
-  //   textFont("Ubuntu");
-  //   text("stroke", windowWidth - 95, 390+75);
-  //   fill(0);
-  //   text("branches \n dimension", windowWidth - 95, 435+75);
-  // pop();
 }
